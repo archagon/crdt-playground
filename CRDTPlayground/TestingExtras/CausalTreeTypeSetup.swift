a57b8af290323b5f2d1661d08c982a74e19ccbe3
Archagon <archagon@gmail.com>
2017-09-25 17:18:13 +0300

    Refactoring…

---
+++ CRDTPlayground/TestingExtras/CausalTreeTypeSetup.swift
@@ -0,0 +1,9 @@
+//
+//  CausalTreeTypeSetup.swift
+//  CRDTPlayground
+//
+//  Created by Alexei Baboulevitch on 2017-9-25.
+//  Copyright © 2017 Alexei Baboulevitch. All rights reserved.
+//
+
+import Foundation
