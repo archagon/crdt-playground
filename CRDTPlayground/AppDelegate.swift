58d711aba65cbe808aa1f04334c46b0cac1b784e
Archagon <archagon@gmail.com>
2018-02-17 01:02:17 -0800

    Cleanup.

--- CRDTPlayground/AppDelegate.swift
+++ CRDTPlayground/AppDelegate.swift
@@ -6,11 +6,6 @@
 //  Copyright © 2017 Alexei Baboulevitch. All rights reserved.
 //
 
-// NEXT: no need to save commit if a) you're already aware of the site so far, b) you're connecting to the last item
-// NEXT: completeWeft() vs. siteLocalWeft()? (i.e. what does my site know so that I can strategically place my commits?)
-// NEXT: overlapping site id bug in hard concurrency demo weave?
-// NEXT: is associatedtype enum w/new data types still decodable in part by older client?
-
 import Cocoa
 //import CRDTFramework_OSX
 
@@ -41,7 +36,6 @@ import Cocoa
 }
 
 // test recorder commands
-// NEXT:  parameters for these cases!
 // TODO: move these elsewhere
 enum TestCommand: TestingRecorderActionId, CustomStringConvertible
 {
